2a316c4935a36c180fcc3b24c3a57f80cc61515e
GRSJ <jgaurav23official@gmail.com>
2026-01-30 23:58:16 +0530

    added create user functionality with custom user model with model tests

---
+++ app/tests/test_models.py
@@ -0,0 +1,56 @@
+"""
+test for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """ Test models """
+
+    def test_create_user_with_email_successfull(self):
+        """ Test creating a user with an email is successfull """
+        email = 'test@example.com'
+        password = 'test@234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test email is normalized for new users. """
+        sample_emails = [
+            ("test1@EXAMPLE.COM", "test1@example.com"),
+            ("Test2@Example.COM", "Test2@example.com"),
+            ("TEST3@EXAMPLE.COM", "TEST3@example.com"),
+            ("test4@example.COM", "test4@example.com"),
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email,
+                password="test123"
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """ Test that creating a user without an email raises a ValueError. """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password="test123"
+            )
+
+    def test_create_superuser(self):
+        """ Test creating a superuser. """
+        user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="test123"
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
